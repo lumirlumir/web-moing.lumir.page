d943f21f6ce622bb48d79bafbca5e22a071cefc1
루밀LuMir <rpfos@naver.com>
2025-04-03 20:13:35 +0900

    chore(*): update root level config files (#239)
    This pull request includes several changes to configuration files and a
    migration from CommonJS to ECMAScript modules. The most important
    changes focus on updating configuration formats and improving
    compatibility with various file types.
    
    Configuration updates:
    
    *
    [`.editorconfig-checker.json`](diffhunk://#diff-ffc189b5d4f8c2eb9a171e067565fbfe9ecba9a5b717d236cbf22497973fc4a3L1-R3):
    Added an empty `Exclude` array to the configuration file.
    *
    [`.github/sync-client.yml`](diffhunk://#diff-93bc202766315b6269beef308a6ad30ed3e86938ddbfa31b49e030f2263695f1L50-R51):
    Updated the `.prettierrc` configuration file extension from `.js` to
    `.cjs` to align with CommonJS standards.
    
    Migration to ECMAScript modules:
    
    *
    [`lint-staged.config.js`](diffhunk://#diff-29c15f0b5d8d1144bd22153c302cde16c2de092d65b122a618cbdb3023336346L1-L8):
    Removed the CommonJS configuration file.
    *
    [`lint-staged.config.mjs`](diffhunk://#diff-7de6c7dbbd1b286aac2271abff52aed05b83379c998f2158f357046f9fd06c7dR1-R8):
    Added a new ECMAScript module configuration file with updated file type
    compatibility and the `--ignore-unknown` flag for Prettier.

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*': [
-    'npx prettier --check',
-    'npx editorconfig-checker -config .editorconfig-checker.json',
-  ],
-  '*.{js,jsx}': 'npx eslint',
-  '*.md': 'npx markdownlint',
-};
