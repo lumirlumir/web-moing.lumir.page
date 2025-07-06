fa4fec078c0ab3706a2684799c28957c0c68a02b
루밀LuMir <rpfos@naver.com>
2025-07-06 15:07:50 +0900

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import Start from '@/pages';
+
+import useScenario from '@/hooks/useScenario';
+import useConfig from '@/hooks/useConfig';
+import useInterview from '@/hooks/useInterview';
+import useTimer from '@/hooks/useTimer';
+
+import FooterL from '@/pages/FooterL';
+import FooterM from '@/pages/FooterM';
+import FooterR from '@/pages/FooterR';
+import HeaderL from '@/pages/HeaderL';
+import HeaderR from '@/pages/HeaderR';
+import Main from '@/pages/Main';
+
 import './App.scss';
 
 /**
@@ -7,5 +19,21 @@ import './App.scss';
  * @returns App
  */
 export default function App() {
-  return <Start />;
+  /* Hooks */
+  const scenario = useScenario();
+  const config = useConfig();
+  const interview = useInterview();
+  const timer = useTimer(interview.submit);
+
+  /* Return */
+  return (
+    <div className="Start">
+      <HeaderL scenario={scenario} config={config} />
+      <HeaderR scenario={scenario} interview={interview} />
+      <Main scenario={scenario} config={config} interview={interview} timer={timer} />
+      <FooterL scenario={scenario} />
+      <FooterM scenario={scenario} timer={timer} />
+      <FooterR scenario={scenario} interview={interview} timer={timer} />
+    </div>
+  );
 }
