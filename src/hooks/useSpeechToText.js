392087130de7038fdf23b4dd24e5599d8c54f441
루밀LuMir <rpfos@naver.com>
2025-08-13 19:00:43 +0900

    refactor: replace `react-speech-recognition` with native speech recognition

--- src/hooks/useSpeechToText.js
+++ src/hooks/useSpeechToText.js
@@ -1,5 +1,43 @@
+/**
+ * @fileoverview Speech Recognition Hook.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API
+ * @see https://github.com/mdn/dom-examples/blob/main/web-speech-api/speech-color-changer/script.js
+ * @see https://stackoverflow.com/questions/75420503/how-to-detect-speech-to-text-support-and-what-is-the-meaning-of-mozspeechrecogni
+ */
+
+/* eslint-disable -- TODO */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
 import { useCallback } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+// --------------------------------------------------------------------------------
+// Helpers
+// --------------------------------------------------------------------------------
+
+const NativeSpeechRecognition =
+  typeof window !== 'undefined' &&
+  (window.SpeechRecognition ||
+    window.webkitSpeechRecognition ||
+    window.mozSpeechRecognition ||
+    window.msSpeechRecognition ||
+    window.oSpeechRecognition);
+
+const isNative = SpeechRecognition => SpeechRecognition === NativeSpeechRecognition;
+
+const isAndroid = /(android)/i.test(
+  typeof navigator !== 'undefined' ? navigator.userAgent : '',
+);
+
+let _browserSupportsSpeechRecognition = !!NativeSpeechRecognition;
+let _browserSupportsContinuousListening =
+  _browserSupportsSpeechRecognition && !isAndroid();
+
+// --------------------------------------------------------------------------------
+// Export
+// --------------------------------------------------------------------------------
 
 export default function useSpeechToText() {
   /* Hooks */
