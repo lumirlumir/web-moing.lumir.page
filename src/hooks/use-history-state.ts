74978757ad3db417b5391025260f18eb8998f275
루밀LuMir <rpfos@naver.com>
2025-08-24 18:10:32 +0900

    wip

--- src/hooks/use-history-state.ts
+++ src/hooks/use-history-state.ts
@@ -5,16 +5,12 @@ import { useCallback, useState } from 'react';
  * @returns
  */
 export default function useHistoryState() {
-  /* Hooks */
-  // useState
   const [historyState, setHistoryState] = useState([]);
 
-  /* Func */
   const addHistory = useCallback(val => {
     setHistoryState(prevState => [...prevState, val]);
   }, []);
 
-  /* Return */
   return {
     historyState,
     addHistory,
