e861c29d035dbb0780809373a62d3e378999c8ba
루밀LuMir <rpfos@naver.com>
2025-09-20 00:07:48 +0900

    wip

--- src/components/typewriter/typewriter.tsx
+++ src/components/typewriter/typewriter.tsx
@@ -154,7 +154,7 @@ export default function Typewriter({
       return undefined;
     }
 
-    /** Minimal helper to emulate `setTimeout` with RAF */
+    /** Minimal helper to emulate `setTimeout` with RAF(RequestAnimationFrame) */
     const setTimeoutRaf = (callback: () => void, delay: number) => {
       const base = performance.now();
 
