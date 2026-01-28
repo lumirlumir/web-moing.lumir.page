517a8f97692ef749133412c882c7eafec9f41f6e
루밀LuMir <rpfos@naver.com>
2026-01-28 14:36:51 +0900

    wip

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,14 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies -- TODO */
 
+import { existsSync } from 'node:fs';
 import { resolve } from 'node:path';
+import { loadEnvFile } from 'node:process';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+if (existsSync(resolve(import.meta.dirname, '.env'))) {
+  loadEnvFile(resolve(import.meta.dirname, '.env'));
+}
 
 export default defineConfig({
   build: {
@@ -15,6 +21,10 @@ export default defineConfig({
           "@import '@/styles/mixins.scss';\n@import '@/styles/variables.scss';",
       },
     },
+  },
+  define: {
+    'process.env.BACKEND_PORT': JSON.stringify(process.env.BACKEND_PORT),
+    'process.env.BACKEND_IP': JSON.stringify(process.env.BACKEND_IP),
   },
   plugins: [react()],
   resolve: {
